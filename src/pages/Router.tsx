295b9b8d800b69c15337420c3aed05903f901406
Vitor Ramires <vmr.rs@hotmail.com>
2024-08-21 16:16:22 -0300

    historic page

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import { Home } from "../../src/pages/Home/index"
-import { Historic } from "./historic"
+import { Historic } from "../../src/pages/Historic/index"
 import { DefaultLayout } from "../layouts/DefaultLayout"
 
 export function Router(){
--- src/pages/historic.tsx
+++
@@ -1,3 +0,0 @@
-export function Historic(){
-    return <p></p>
-}
