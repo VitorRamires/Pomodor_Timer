9c686b68cb5a38e9b10c109f22ec7be4c3fb4db5
Vitor Ramires <vmr.rs@hotmail.com>
2024-08-23 15:58:36 -0300

    formatting start date

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,8 @@
 import { HistoryContainer, HistoryList, Status } from './styles'
 import { CyclesContext } from '../../context/cyclesContext'
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 // ------------------------------------------------------------------------------ //
 
@@ -27,7 +29,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluido</Status>
