2be5a481198ea535f9b159cbebf75df93eb8ca55
Vitor Ramires <vmr.rs@hotmail.com>
2024-09-24 19:51:37 -0300

    add nodules

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,4 +1,5 @@
-import { FormContainer, MinutesAmountInput, TaskInput } from '../styles'
+
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
 import { useContext } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { CyclesContext } from '../../../../contexts/CyclesContext'
