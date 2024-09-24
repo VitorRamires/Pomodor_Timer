878eb0c3661b2841ce66f283bc9328df79aa4cab
Vitor Ramires <vmr.rs@hotmail.com>
2024-09-24 19:06:38 -0300

    fixes

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,4 +1,4 @@
-import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { FormContainer, MinutesAmountInput, TaskInput } from '../styles'
 import { useContext } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { CyclesContext } from '../../../../contexts/CyclesContext'
